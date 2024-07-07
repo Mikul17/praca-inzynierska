317e0264134a94e004f12dc0a29b7e0d2f73cdc7
Mikul17 <84858817+Mikul17@users.noreply.github.com>
2024-07-07 17:21:44 +0200

    init backend

---
+++ Rpq-solver/src/main/java/org/mikul17/rpq/RpqSolverApplication.java
@@ -0,0 +1,13 @@
+package org.mikul17.rpq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpqSolverApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(RpqSolverApplication.class, args);
+    }
+
+}
---
+++ Rpq-solver/src/test/java/org/mikul17/rpq/RpqSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mikul17.rpq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpqSolverApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
