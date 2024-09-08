f305f2480f0d760f8477465acd19096709d51605
Miłosz Mikulski <84858817+Mikul17@users.noreply.github.com>
2024-09-08 02:37:39 +0200

    feat: added algorithm selector (#8)

--- frontend/src/app/components/AlgorithmSelector.tsx
+++ frontend/src/app/components/AlgorithmSelector.tsx
@@ -1,27 +1,39 @@
 import { Select, SelectItem } from "@nextui-org/select";
+import {
+  useAlgorithm,
+  AlgorithmType,
+  AlgorithmDisplayNames,
+  AlgorithmTypeKeys,
+} from "../providers/context/AlgorithmContext";
 
 export default function AlgorithmSelector() {
-  const algorithms = [
-    "Simulated Annealing",
-    "Tabu Search",
-    "Schrage Algorithm",
-    "Carlier Algorithm",
-  ];
+  const { currentAlgorithm, setCurrentAlgorithm } = useAlgorithm();
 
   return (
-    <Select
-      className="w-64 !rounded-l-none !rounded-r-s"
+    <Select<typeof AlgorithmType>
+      className="w-64 !   rounded-l-none !rounded-r-s"
       classNames={{
         trigger: "!rounded-l-none !rounded-r-2xl text-black",
-        listbox: "!rounded-l-none !rounded-r-md",
-        spinner: "text-black"
+        spinner: "text-black",
       }}
       size="lg"
       placeholder="Select an algorithm"
+      value={currentAlgorithm}
+      onChange={(e) => setCurrentAlgorithm(e.target.value as AlgorithmTypeKeys)}
+      aria-label="Select an algorithm"
+      disabledKeys={[currentAlgorithm]}
     >
-      {algorithms.map((algorithm) => (
-        <SelectItem key={algorithm} value={algorithm}>
-          {algorithm}
+      {Object.values(AlgorithmType).map((algorithm) => (
+        <SelectItem
+          key={algorithm}
+          value={algorithm}
+          className={
+            algorithm === currentAlgorithm
+              ? "bg-black text-white"
+              : "text-black"
+          }
+        >
+          {AlgorithmDisplayNames[algorithm]}
         </SelectItem>
       ))}
     </Select>
---
+++ frontend/src/app/providers/context/AlgorithmContext.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+} from "react";
+
+export const AlgorithmType = {
+  SimulatedAnnealing: "SimulatedAnnealing",
+  TabuSearch: "TabuSearch",
+  SchrageAlgorithm: "SchrageAlgorithm",
+  CarlierAlgorithm: "CarlierAlgorithm",
+} as const;
+
+export type AlgorithmTypeKeys = keyof typeof AlgorithmType;
+
+export const AlgorithmDisplayNames: Record<AlgorithmTypeKeys, string> = {
+  SimulatedAnnealing: "Simulated Annealing",
+  TabuSearch: "Tabu Search",
+  SchrageAlgorithm: "Schrage Algorithm",
+  CarlierAlgorithm: "Carlier Algorithm",
+};
+
+interface AlgorithmContextType {
+  currentAlgorithm: AlgorithmTypeKeys;
+  setCurrentAlgorithm: React.Dispatch<React.SetStateAction<AlgorithmTypeKeys>>;
+}
+
+const AlgorithmContext = createContext<AlgorithmContextType | undefined>(
+  undefined
+);
+
+export const useAlgorithm = (): AlgorithmContextType => {
+  const context = useContext(AlgorithmContext);
+  if (context === undefined) {
+    throw new Error("useAlgorithm must be used within an AlgorithmProvider");
+  }
+  return context;
+};
+
+interface AlgorithmProviderProps {
+  children: ReactNode;
+}
+
+export const AlgorithmProvider: React.FC<AlgorithmProviderProps> = ({
+  children,
+}) => {
+  const [currentAlgorithm, setCurrentAlgorithm] =
+    useState<AlgorithmTypeKeys>("SimulatedAnnealing");
+
+  return (
+    <AlgorithmContext.Provider
+      value={{ currentAlgorithm, setCurrentAlgorithm }}
+    >
+      {children}
+    </AlgorithmContext.Provider>
+  );
+};
