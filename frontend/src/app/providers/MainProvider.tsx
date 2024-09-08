72972bea932a87c9b21736f0918e854c82d0cfd6
Mikul17 <84858817+Mikul17@users.noreply.github.com>
2024-09-08 02:36:51 +0200

    feat: added algorithm selector

--- frontend/src/app/providers/MainProvider.tsx
+++ frontend/src/app/providers/MainProvider.tsx
@@ -1,9 +1,12 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { AlgorithmProvider } from "./context/AlgorithmContext";
 
 export function MainProvider({children}: { children: React.ReactNode }) {
     return (
         <NextUIProvider>
-            {children}
+            <AlgorithmProvider>
+                {children}
+            </AlgorithmProvider>
         </NextUIProvider>
     )
 }
